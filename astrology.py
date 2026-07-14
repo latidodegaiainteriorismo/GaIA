ba1c80ab304ec34d3f3d7d7638db6a83b6bb6f0f
latidodegaiainteriorismo <latidodegaiainteriorismo@gmail.com>
2026-07-14 13:41:48 +0200

    Update astrology.py

--- astrology.py
+++ astrology.py
@@ -27,6 +27,7 @@ from astro_core.birth_chart import calculate_birth_chart
 from astro_core.transits import get_current_transits_report, get_transits_report_for_date, get_planet_positions_at, calculate_transit_aspects
 from astro_core.models import BirthChart, PlanetPosition, HouseCusp, Aspect
 from astro_core.chart_svg import render_birth_chart_svg, render_transit_chart_svg
+from astro_core.generalidades import calculate_generalidades
 
 logger = logging.getLogger(__name__)
 
